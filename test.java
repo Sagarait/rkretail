917e5bd15d26697d1b83b1b67fe351ed36837f0e
tom <tom@example.com>
2018-07-14 10:29:05 +0000

    cleanup

--- test.java
+++
@@ -1,6 +0,0 @@
-Adding new file to the tommm
-tom
-
-adding on more  
-
-now jerry added and making commit 
--- test1.java
+++
@@ -1,6 +0,0 @@
-new added for abhhi 
-new file regarding the stash
-new file added for patching
-
-for braching purpose
-
